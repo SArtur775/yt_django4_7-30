a322c774d19679b7de671c559c5fe390ecf5c8ab
Vozirk <Vozirkutsk@yandex.ru>
2024-07-23 11:29:06 +1000

    add image

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='название продукта')
     price = models.IntegerField(verbose_name='цена продукта')
     description = models.CharField(max_length=250, verbose_name='описание продукта')
+    image = models.ImageField(blank=True, upload_to='my_images')
     
     def __str__(self):
         return f"{self.name} по цене {self.price} "
